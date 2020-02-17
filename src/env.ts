b3bbde8cb72acf7da720d9028d33bb78b65fa496
arbassic <arbassic@gmail.com>
2020-02-18 00:01:19 +0100

    add env to provide comfortable way to access environment variables

---
+++ src/env.ts
@@ -0,0 +1,13 @@
+import * as dotenv from 'dotenv';
+const result = dotenv.config();
+if (result.error) throw Error("Missing .env file");
+
+const env = {
+  NODE_ENV: process.env.NODE_ENV,
+  PORT: process.env.PORT,
+  isProd: process.env.NODE_ENV == 'production',
+  isTest: process.env.NODE_ENV == 'test',
+  isDev: process.env.NODE_ENV == 'development',
+};
+
+export default env;
