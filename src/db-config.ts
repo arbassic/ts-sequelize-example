69add1ef29ee88715904b1d847ad307b3b05c3bc
arbassic <arbassic@gmail.com>
2020-02-19 09:59:29 +0100

    Move db config to .env

--- src/db-config.ts
+++ src/db-config.ts
@@ -1,11 +1,13 @@
 import { Dialect, Options } from "sequelize";
 import { logger } from "logger";
+import env from "env";
 
 const Config: Options = {
-  database: "sequelize_test_db",
-  username: "root",
-  password: null,
-  host: "localhost",
+  database: env.DB_DATABASE,
+  username: env.DB_USERNAME,
+  password: env.DB_PASSWORD,
+  host: env.DB_HOST,
+  port: env.DB_PORT,
   dialect: "mariadb" as Dialect,
   dialectOptions: {
     timezone: process.env.db_timezone
--- src/env.ts
+++ src/env.ts
@@ -5,6 +5,13 @@ if (result.error) throw Error("Missing .env file");
 const env = {
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
+
+  DB_DATABASE: process.env.DB_DATABASE,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: parseInt(process.env.DB_PORT) || null,
+
   isProd: process.env.NODE_ENV == 'production',
   isTest: process.env.NODE_ENV == 'test',
   isDev: !process.env.NODE_ENV || process.env.NODE_ENV == 'development',
